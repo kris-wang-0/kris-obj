6c7ca665ed1b5da9f8fc7bffd3da71ac0c5daf90
kris-wang-0 <68373491+kris-wang-0@users.noreply.github.com>
2022-01-10 08:34:08 +0800

    Update funs.h

--- AddressBook/funs.h
+++ AddressBook/funs.h
@@ -3,6 +3,8 @@
 #include <string>
 #include <iostream>
 #include <unordered_map>
+#include <Windows.h>
+
 #define LEN 1000
 #define UINT unsigned int
 using namespace std;
@@ -16,11 +18,12 @@ struct AddressBook {
 };
 
 
-
-void addContacts(UINT *addc,AddressBook oneAdd);
+void input(AddressBook *oneAddress);
+void addContacts(UINT *addc,AddressBook oneAddress);
 void showContacts(UINT addc);
 void deleteContacts(string name,UINT *addc);
 void findContacts(string name);
 void modifyContacts(string name);
 void clearContacts(UINT *addc);
 void testAdd(UINT* addc);
+void show(UINT* addc);
